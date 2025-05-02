5af02908e3f8d1882574999d4a60930220e5e79a
Tulga0206 <tulgaag925@gmail.com>
2025-05-02 16:09:09 +0800

    d

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BD_visual_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form form2 = new Form2();
+            form2.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BD_visual_
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            this.imageList1 = new System.Windows.Forms.ImageList();
+            this.imageList1.Images.Add("available", Image.FromFile("gray.png"));    // боломжтой
+            this.imageList1.Images.Add("booked", Image.FromFile("red.png"));         // захиалагдсан
+            this.imageList1.Images.Add("selected", Image.FromFile("green.png"));     // сонгосон
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is PictureBox)
+                {
+                    PictureBox suudal = (PictureBox)ctrl;
+                    suudal.Image = imageList1.Images["available"]; // эхлээд саарал
+                    suudal.SizeMode = PictureBoxSizeMode.StretchImage;
+                    suudal.Click += suudal_Click;
+                }
+            }
+        }
+
+        private void suudal_Click(object sender, EventArgs e)
+        {
+            PictureBox clickedSeat = (PictureBox)sender;
+
+            // ☝️ эхлээд шалгана
+            if ((string)clickedSeat.Tag == "reserved")
+                return;
+
+            // ✔ боломжтой бол ногоон болгоно
+            if (clickedSeat.Image == imageList1.Images["available"])
+            {
+                clickedSeat.Image = imageList1.Images["selected"];
+            }
+            else if (clickedSeat.Image == imageList1.Images["selected"])
+            {
+                clickedSeat.Image = imageList1.Images["available"];
+            }
+        }
+
+        private void suudal3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void suudal2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
